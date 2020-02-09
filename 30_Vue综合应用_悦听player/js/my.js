6c9107cb647782995da178bdf4ceecab0d28191d
I306454 <jiabin.zheng01@sap.com>
2020-02-09 15:05:54 +0800

    start player

---
+++ 30_Vue综合应用_悦听player/js/my.js
@@ -0,0 +1,47 @@
+/*
+  1:歌曲搜索接口
+    请求地址:https://autumnfish.cn/search
+    请求方法:get
+    请求参数:keywords(查询关键字)
+    响应内容:歌曲搜索结果
+
+  2:歌曲url获取接口
+    请求地址:https://autumnfish.cn/song/url
+    请求方法:get
+    请求参数:id(歌曲id)
+    响应内容:歌曲url地址
+  3.歌曲详情获取
+    请求地址:https://autumnfish.cn/song/detail
+    请求方法:get
+    请求参数:ids(歌曲id)
+    响应内容:歌曲详情(包括封面信息)
+  4.热门评论获取
+    请求地址:https://autumnfish.cn/comment/hot?type=0
+    请求方法:get
+    请求参数:id(歌曲id,地址中的type固定为0)
+    响应内容:歌曲的热门评论
+  5.mv地址获取
+    请求地址:https://autumnfish.cn/mv/url
+    请求方法:get
+    请求参数:id(mvid,为0表示没有mv)
+    响应内容:mv的地址
+*/
+var app = new Vue({
+    el: "#player",
+    data: {
+        query: "",
+        musicList: [],
+    },
+    methods: {
+        searchMusic: function() {
+            // console.log(this.query);
+            var that = this;
+            axios.get("https://autumnfish.cn/search?keywords=" + this.query)
+                .then(function(response) {
+                    // console.log(response);
+                    console.log(response.data.result.songs);
+                    that.musicList = response.data.result.songs;
+                }, function(err) {})
+        }
+    }
+})
