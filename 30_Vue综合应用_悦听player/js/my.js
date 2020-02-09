509dcd1bb771a9879e7387b7a290d09b8f7f628e
I306454 <jiabin.zheng01@sap.com>
2020-02-09 16:41:48 +0800

    finish guid

--- 30_Vue综合应用_悦听player/js/my.js
+++ 30_Vue综合应用_悦听player/js/my.js
@@ -31,6 +31,12 @@ var app = new Vue({
     data: {
         query: "",
         musicList: [],
+        musicUrl: "",
+        musicCover: "",
+        hotComments: [],
+        isPlaying: false,
+        isShow: false,
+        mvUrl: ""
     },
     methods: {
         searchMusic: function() {
@@ -42,6 +48,48 @@ var app = new Vue({
                     console.log(response.data.result.songs);
                     that.musicList = response.data.result.songs;
                 }, function(err) {})
+            this.query = "";
+        },
+        playMusic: function(musicId) {
+            // console.log(musicId);
+            var that = this;
+            axios.get("https://autumnfish.cn/song/url?id=" + musicId)
+                .then(function(response) {
+                    // console.log(response.data.data[0].url);
+                    that.musicUrl = response.data.data[0].url;
+                }, function(err) {});
+
+            axios.get("https://autumnfish.cn/song/detail?ids=" + musicId)
+                .then(function(response) {
+                    // console.log(response);
+                    that.musicCover = response.data.songs[0].al.picUrl;
+                }, function(err) {});
+
+            axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + musicId)
+                .then(function(response) {
+                    console.log(response);
+                    that.hotComments = response.data.hotComments;
+                }, function(err) {})
+        },
+        play: function() {
+            this.isPlaying = true;
+            // console.log("play");
+        },
+        pause: function() {
+            this.isPlaying = false;
+            // console.log("pause");
+        },
+        playMV: function(mvId) {
+            var that = this;
+            axios.get("https://autumnfish.cn/mv/url?id=" + mvId)
+                .then(function(response) {
+                    // console.log(response.data.data.url);
+                    that.isShow = true;
+                    that.mvUrl = response.data.data.url;
+                }, function(err) {})
+        },
+        hide: function() {
+            this.isShow = false;
         }
     }
 })
